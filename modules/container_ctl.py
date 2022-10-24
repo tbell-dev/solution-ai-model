5ed4e1ffab46886d7c3b0aafed2248602fe95108
kangmin5133 <112845890+kangmin5133@users.noreply.github.com>
2022-10-24 15:46:54 +0900

    Update container_ctl.py

--- modules/container_ctl.py
+++ modules/container_ctl.py
@@ -1,10 +1,40 @@
 import docker
 
+def get_container_list():
+    name_list = []
+    client = docker.from_env()
+    for i in client.containers.list():
+        name = i.name
+        name_list.append(name)
+    return name_list
+
+def get_port_usage():
+    client = docker.from_env()
+    ports_in_use = {}
+    for i in client.containers.list():
+        container = client.containers.get(i.id)
+        ports_in_use[str(i.name)] = list(container.ports.items())[0][-1][0]["HostPort"]
+    return ports_in_use
+
+def stop_container(name):
+    client = docker.from_env()
+    for i in client.containers.list():
+        container_name = i.name
+        if name == container_name :
+            i.stop()
+
+def rm_container(name):
+    client = docker.from_env()
+    for i in client.containers.list():
+        container_name = i.name
+        if name == container_name :
+            i.remove()
+            
 def trainserver_start(dataset_path,labeling_type,project_name):
     client = docker.from_env()
     container = client.containers.run(
     image = 'tbelldev/sslo-ai:t-v0.1',
-    name = "trainserver",
+    name = "trainserver_"+str(project_name),
     detach=True,
     runtime="nvidia",
     device_requests=[
@@ -21,3 +51,23 @@ def trainserver_start(dataset_path,labeling_type,project_name):
     )
     
     return container
+
+def inference_server_start(model_repo_path,port,project_name):
+    client = docker.from_env()
+    container = client.containers.run(
+    image = 'tbelldev/sslo-ai:i-v0.1',
+    name = "inference_server_"+str(project_name),
+    detach=True,
+    runtime="nvidia",
+    device_requests=[
+        docker.types.DeviceRequest(device_ids=["0"], capabilities=[["gpu"]])
+    ],
+    ports = {'8000/tcp': port},
+    shm_size ="1G",
+    volumes = {model_repo_path:{'bind':"/models",'mode':"rw"}},
+    command = "tritonserver --model-repository=/models",
+    remove = True
+    )
+    
+    return container
+
