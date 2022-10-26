55d93283478bfcdb837d26ca895e8621d0953f74
kangmin5133 <112845890+kangmin5133@users.noreply.github.com>
2022-10-26 17:38:17 +0900

    Update model_validation.py

--- modules/model_validation.py
+++ modules/model_validation.py
@@ -15,18 +15,23 @@ def get_loss(metric_path):
 
 def model_validation(model_repo,current_prj_name):
     prjs = [i for i in glob(model_repo+"/*") if current_prj_name in i.split("/")[-1]]
-    print(prjs)
-    latest_result = prjs[-1]
-    previous_result = prjs[:-1]
-    latest_loss = get_loss(latest_result)["total_loss"]
-    
-    previous_losses = []
-    for i in range(len(previous_result)):
-        previous_losses.append(get_loss(previous_result[i])["total_loss"])
-            
-    if min(previous_losses) < latest_loss : 
-        min_loss = min(previous_losses)
-        idx = previous_losses.index(min_loss)
-        return previous_result[idx]+"/model_final.pth"
-    elif min(previous_losses) >= latest_loss : 
-        return latest_result+"/model_final.pth"
+    if len(prjs) == 1 : 
+        return prjs[0]+"/model_final.pth"
+    elif len(prjs) == 0 :
+        return model_repo+"/"+current_prj_name+"/model_final.pth"
+    else :
+        latest_result = prjs[-1]
+        previous_result = prjs[:-1]
+        latest_loss = get_loss(latest_result)["total_loss"]
+        
+        previous_losses = []
+        for i in range(len(previous_result)):
+            previous_losses.append(get_loss(previous_result[i])["total_loss"])
+                
+        if min(previous_losses) < latest_loss : 
+            min_loss = min(previous_losses)
+            idx = previous_losses.index(min_loss)
+            return previous_result[idx]+"/model_final.pth"
+        
+        elif min(previous_losses) >= latest_loss : 
+            return latest_result+"/model_final.pth"
