401a4b1828222d3cad748d95f1c499166ec3404a
kangmin5133 <112845890+kangmin5133@users.noreply.github.com>
2022-11-03 17:26:11 +0900

    Update inference.py
    fix:delete unuse pakage import & unuse func

--- inference.py
+++ inference.py
@@ -1,22 +1,3 @@
-import torch, detectron2
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
-
-# import some common libraries
-import numpy as np
-import os, json, cv2, random
-
-
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-
-import cv2
-from matplotlib import pyplot as plt
-
 #custom helper module
 from modules.post_proccessing import *
 
@@ -25,75 +6,23 @@ from modules.formatter import *
 
 # triton serving client
 from modules.triton_serving import *
-
-
-def get_base_model(label_type:str):
     
-    cfg = get_cfg()
-    
-    if label_type == "bbox":
-        cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
-    elif label_type == "polygon":
-        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-    elif label_type == "segment":
-        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-        
-    return cfg       
-
-def inference_local(image,class_name:list,label_type = "bbox"):
-    
-    cfg = get_base_model(label_type)
-    
-    predictor = DefaultPredictor(cfg)
-    outputs = predictor(image)
-    
-    filtered_outputs = inference_class_filter(outputs,class_name) # filtered_outputs : list (ex. [Instance(),Instance(),Instance()])
-    
-    if label_type == "bbox":
-        boxes = get_boxes(filtered_outputs)
-        return boxes
-    
-    elif label_type == "polygon":
-        polygon = get_polygon_from_mask(filtered_outputs)
-        return polygon
-    
-    elif label_type == "segment":
-        binary_mask = get_mask_from_boolen(filtered_outputs)
-        return binary_mask
-    
-def inference_triton(image,class_name:list,port=8000,label_type = "bbox"):
+def inference_triton(image_path,model_name:str,class_list:list,confidence,port=8000,label_type = "bbox"):
     # inference using triton serving container , with httpclient 
-    confidence = 0.60
-    
     if label_type == "bbox":
         task_type = "od"
-        model_name = "faster_rcnn"
+        # model_name = "faster_rcnn"
         pred = inference(model_name,image_path,task_type,port=port)
-        result = infer_result_filter(pred,task_type,confidence,"person")
+        result = infer_result_filter(pred,task_type,confidence,class_list)
         
     elif label_type == "polygon" or label_type == "segment":
         task_type = "seg"
-        model_name = "infer_pipeline"
+        # model_name = "infer_pipeline"
         pred = inference(model_name,image_path,task_type,port=port)
-        result = infer_result_filter(pred,task_type,confidence,"person")
-                
-    # elif label_type == "segment":
-    #     task_type = "seg"
-    #     model_name = "infer_pipeline"
-    #     pred = inference(model_name,image_path,task_type)
-    #     result = infer_result_filter(pred,task_type,confidence,"person")
+        result = infer_result_filter(pred,task_type,confidence,class_list)
     
     response_data = coco_format_inverter(result)
-    
+    # return result
     return response_data
 
 def parse_args():
@@ -104,6 +33,7 @@ def parse_args():
     parser.add_argument('--conf', type=float , default=0.6)
     parser.add_argument('--class_name', type=str ,default="person")
     parser.add_argument('--serving-port', type=int ,default=8000)
+    parser.add_argument('--serving-host', type=str ,default='localhost')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -114,8 +44,9 @@ if __name__ == '__main__':
     confidence = args.conf
     class_name =  args.class_name
     port = int(args.serving_port)
+    host = str(args.serving_host)
     
-    pred = inference(model_name,image_path,task_type,port=port)
+    pred = inference(model_name,image_path,task_type,port=port,host=host)
     result = infer_result_filter(pred,task_type,confidence,class_name)
     response_data = coco_format_inverter(result)
     
