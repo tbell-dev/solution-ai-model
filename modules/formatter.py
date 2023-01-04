d3fe82416525ae973bb534ae9b51279ae3202a78
kangmin5133 <112845890+kangmin5133@users.noreply.github.com>
2023-01-04 17:55:40 +0900

    Update formatter.py
    add : add batch mode coco inverter

--- modules/formatter.py
+++ modules/formatter.py
@@ -42,7 +42,7 @@ def create_coco_dict_seg(image,segmentations,bbox,id,idx):
     
     return json_data
 
-def create_coco_dict_seg_v2(image,mask,bbox,id,idx,score):
+def create_coco_dict_seg_v2(image,mask,bbox,id,idx,score,image_id = 0):
     '''
     only creates coco dataset annotation field 
     '''
@@ -71,18 +71,10 @@ def create_coco_dict_seg_v2(image,mask,bbox,id,idx,score):
             if len(segmentation) == 0: #check again if segmentations are in list
                 continue
             if (width * height) > 100 :
-                
-                # result_list.append({'segmentation': segmentation,
-                #                 'area': width * height,
-                #                     'image_id': 0,
-                #                     'iscrowd':0,
-                #                     'bbox': [xmin,ymin,width,height],
-                #                     "category_id": id,
-                #                     "id": idx})
                 data = {
                         'segmentation': segmentation,
                         'area': width * height,
-                        'image_id': 0,
+                        'image_id': image_id,
                         'iscrowd':0,
                         'bbox': [xmin,ymin,width,height],
                         "category_id": id,
@@ -92,7 +84,7 @@ def create_coco_dict_seg_v2(image,mask,bbox,id,idx,score):
     
     return data
 
-def create_coco_dict_od(bbox,id,idx,score):
+def create_coco_dict_od(bbox,id,idx,score,image_id = 0):
     '''
     only creates coco dataset annotation field 
     '''
@@ -106,7 +98,7 @@ def create_coco_dict_od(bbox,id,idx,score):
     #                     'iscrowd':0
     #                     })
     data = {'id': idx,
-            'image_id': 0,
+            'image_id': image_id,
             'category_id': id,
             'bbox':  list(map(int,bbox.tolist())),
             'area': width * height,
@@ -129,5 +121,31 @@ def coco_format_inverter(result):
         for i in range(len(result["bboxes__0"])):
             data = create_coco_dict_od(result["bboxes__0"][i],result["classes__1"][i],i,result["scores__2"][i])
             json_data["annotations"].append(data)
+    return json_data
+
+def coco_format_inverter_batch(result_list,image_list):
+    json_data = {}
+    json_data["annotations"] = []
+    for r in range(len(result_list)):
+        file_name = str(image_list[r]).split(" ")[1].replace("'","").split(".")[0]
+        if "MASKS" in list(result_list[r].keys()):
+            for i in range(len(result_list[r]["MASKS"])):
+                binary_mask = np.where(result_list[r]["MASKS"][i] > 0,255,0)
+                data = create_coco_dict_seg_v2(binary_mask,
+                                               result_list[r]["MASKS"][i],
+                                               result_list[r]["BBOXES"][i],
+                                               result_list[r]["CLASSES"][i],
+                                               i,
+                                               result_list[r]["SCORES"][i],
+                                               image_id = file_name)
+                json_data["annotations"].append(data)
+        else:
+            for i in range(len(result_list[r]["bboxes__0"])):
+                data = create_coco_dict_od(result_list[r]["bboxes__0"][i],
+                                           result_list[r]["classes__1"][i],
+                                           i,
+                                           result_list[r]["scores__2"][i],
+                                           image_id = file_name)
+                json_data["annotations"].append(data)
     return json_data
         
